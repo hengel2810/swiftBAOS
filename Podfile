35d040ba5528ddfd21e809678a45988fefdc8aca
hengel2810 <hengel2810@gmail.com>
2015-12-09 22:57:59 +0100

    Init Commit

---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'SwiftHTTP', '~> 1.0.0'
+pod 'Starscream', '~> 1.0.0'
---
+++ swiftBAOS/swiftBAOS.h
@@ -0,0 +1,19 @@
+//
+//  swiftBAOS.h
+//  swiftBAOS
+//
+//  Created by Henrik Engelbrink on 09.12.15.
+//  Copyright © 2015 Henrik Engelbrink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swiftBAOS.
+FOUNDATION_EXPORT double swiftBAOSVersionNumber;
+
+//! Project version string for swiftBAOS.
+FOUNDATION_EXPORT const unsigned char swiftBAOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftBAOS/PublicHeader.h>
+
+
